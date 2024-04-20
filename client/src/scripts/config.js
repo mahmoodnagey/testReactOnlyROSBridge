d84969be919ade5f9f27c44808aa416035e904f0
Mahmoud Nagy <mahmoud.nagyabosrea@gmail.com>
2024-04-20 16:20:14 +0200

    build react app and add vpn ip

--- client/src/scripts/config.js
+++ client/src/scripts/config.js
@@ -1,7 +1,8 @@
 const Config = {
     // ROSBRIDGE_SERVER_IP: "fc94:b376:aa06:24af:52b8:62c2:d295:1be1", // VPN test ip address
     // ROSBRIDGE_SERVER_IP: "localhost",
-    ROSBRIDGE_SERVER_IP: "192.168.1.102", // Abdelrahman device ip address
+    ROSBRIDGE_SERVER_IP: "abdxi",
+    // ROSBRIDGE_SERVER_IP: "192.168.1.102", // Abdelrahman device ip address
     ROSBRIDGE_SERVER_PORT: "9090",
     RECONNECTION_TIMER: 3000,
     CMD_VEL_TOPIC: "/turtle1/cmd_vel",
